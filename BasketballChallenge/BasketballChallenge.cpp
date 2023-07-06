9feb41ac7ee1809776940637499ac27df4bb23c1
patrick-friel <121344044+patrick-friel@users.noreply.github.com>
2023-07-06 09:40:47 -0600

    organized with functions

--- BasketballChallenge/BasketballChallenge.cpp
+++ BasketballChallenge/BasketballChallenge.cpp
@@ -1,111 +1,180 @@
 // BasketballChallenge.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 #include <time.h>
-
 using namespace std;
 
-// add a delay after 'calculating...'
 // add defense
-// add visuals to show angles and distance
-// add switch
+// ben = add visuals
 // add header file
-// start organizing with functions
 // add for loop
 // add templated function and recursion
+// pass by refs
 
+
+int answer;
+double answer2;
+int score = 0;
+bool isCorrect;
+int rand1 = 1 + rand() % 10;
+int prob1 = 1 + rand() % 5;
+
+int StartMenu();
+void PlayGame();
+int PlayerOption();
+void FirstOffensivePosession();
+void SecondOffensivePosession();
+void ThirdOffensivePosession();
 
 int main()
 {
-    int answer;
-    double answer2;
-    int score = 0;
-    bool isCorrect;
     srand((unsigned int)time(0));
-    int rand1 = 1 + rand() % 11;
-    int prob1 = 1 + rand() % 6;
-    
 
-    // game begins with player having the ball
-    cout << "The game begins! You are " << rand1 << " feet away from the basket and your teammate passes you the ball.You step back 2 feet and take a shot." << endl; 
-    cout << "How far away from the goal in feet are you ? "  << endl;
-    cin >> answer;
-    if (answer == rand1 + 2)
+    int selection = StartMenu();
+
+    switch (selection)
     {
-        isCorrect = true;
-        if (isCorrect)
+        case 1:
         {
-            score++;
-            cout << "You scored! You now have " << score << " point." << endl;
+            PlayGame();
+            break;
+        }
+        case 2:
+        {
+            cout << "Goodbye!";
+            break;
+        }
+        default:
+        {
+            cout << "Invalid Input";
+            break;
         }
     }
-    else
+}
+
+int StartMenu()
+{
+    int selection;
+    cout << "Welcome to the basketball game." << endl;
+    cout << "1) Play" << endl;
+    cout << "2) Quit" << endl;
+    cin >> selection;
+
+    while (selection != 1 && selection != 2)
     {
-        isCorrect = false;
-        if (!isCorrect)
-        {
-            cout << "You missed! Your score remains at " << score << " points." << endl;
-        }       
+        cout << "Please try again." << endl;
+        cin >> selection;
     }
-      
+    return selection;
+}
+
+void PlayGame()
+{
+    // game begins with the option of 2 or 3 point shot and simple math
+    FirstOffensivePosession();
+
     // calc the hypotenuse of a right triangle
-    cout << "You are 4 feet away form the basket. You dribble 3 feet to the left and take a shot. How far away from the basket are you? ";
-    cin >> answer;
-    if (answer == 5)
-    {
-        isCorrect = true;
-        if (isCorrect)
-        {
-            score++;
-            cout << "You scored! You now have " << score << " points." << endl;
-        }
-    }
-    else
-    {
-        isCorrect = false;
-        if (!isCorrect)
-        {
-            cout << "You missed! Your score remains at " << score << " points." << endl;
-        }
-    }
+    SecondOffensivePosession();
 
-    // add two options for the player with while loop and probability for optioni 1
-    cout << "You have been given the ball at the top of the 3 point line which is 22 feet from the basket. You can either shoot and score 3 points with a probability of making it at 20%, or dribble forward 2 feet and right 2 feet with a 80% chance of making it but only scoring 2 point. (and dont forget you will have to calculate the distance of the new position)" << endl;
-    cout << "Do you wish to go with option 1 or 2? ";
-    cin >> answer;
-    while (answer != 1 && answer != 2)
+    ThirdOffensivePosession();
+        
+}
+
+int PlayerOption()
+{
+    int selection;
+    cout << "What would you like to do?" << endl;
+    cout << "1) 2 point attempt" << endl;
+    cout << "2) 3 point attempt" << endl;
+    cin >> selection;
+    while (selection != 1 && selection != 2)
     {
-        cout << "Please try again. ";
-        cin >> answer;        
+        cout << "Please try again: ";
+        cin >> selection;
     }
-    if (answer == 1)
+    return selection;
+}
+
+void FirstOffensivePosession()
+{
+    cout << "The game begins and you are on offense. You can either shoot a 2 point shot with a 60% of making it or shoot a 3 point shot with a 40% of making it." << endl;
+    
+    int selection = PlayerOption();
+
+    switch (selection)
     {
-        cout << "Calculating probability..." << endl;
-        if (prob1 == 1)
+        case 1:
         {
-            score++;
-            score++;
-            score++;
-            cout << "You scored! You now have " << score << " points." << endl;
+            cout << "You are " << rand1 << " feet away from the basket and your teammate passes you the ball.You step back 2 feet and take a shot." << endl;
+            cout << "How far away from the goal in feet are you? " << endl;
+            cin >> answer;
+            if (answer == rand1 + 2)
+            {
+                cout << "Correct! Calculating probability..." << endl;
+                if (prob1 > 2)
+                {
+                    score++;
+                    score++;
+                    cout << "You scored! You now have " << score << " point." << endl;
+                }
+                else
+                {
+                    cout << "You missed!" << endl;
+                }
+            }
+            else
+            {
+                cout << "You missed! Your score remains at " << score << " points." << endl;
+            }
+            break;
         }
-        else
+        case 2:
         {
-            cout << "You missed!" << endl;
+            cout << "You are 10 feet away from the basket. You pass the ball directly behind you 13 feet to a teammate who takes a 3 point shot. How far is he from the basket?" << endl;
+            cin >> answer;
+            if (answer == 23)
+            {
+                cout << "Correct! Calculating probability..." << endl;
+                if (prob1 < 3)
+                {
+                    score++;
+                    score++;
+                    score++;
+                    cout << "You scored! You now have " << score << " points.";
+                }
+                else
+                {
+                    cout << "You missed!" << endl;
+                }
+            }
+            else
+            {
+                cout << "You missed!" << endl;
+            }
+            break;
         }
     }
-    if (answer == 2)
+}
+
+void SecondOffensivePosession()
+{
+    cout << "Second posession: " << endl;
+    int selection = PlayerOption();
+    switch (selection)
     {
-        cout << "You have dribbled to the new position and take the shot. What distance are you from the basket in feet? (please include 1 decimal in your answer.)";
-        cin >> answer2;
-        if (answer2 == 20.1)
+    case 1:
+    {
+        cout << "You are 4 feet away form the basket. You dribble 3 feet to the left and take a shot. How far away from the basket are you? ";
+        cin >> answer;
+        if (answer == 5)
         {
-            cout << "You are correct! Calculating probability..." << endl;
-            if (prob1 != 1)
+            cout << "Correct! Calculating probability..." << endl;
+            if (prob1 > 2)
             {
                 score++;
                 score++;
-                cout << "You score! You now have " << score << " points." << endl;
+                cout << "You scored! You now have " << score << " points." << endl;
             }
             else
             {
@@ -114,9 +183,93 @@ int main()
         }
         else
         {
-            cout << "Incorrect! Your score remains at " << score << " points." << endl;
+            cout << "You missed! Your score remains at " << score << " points." << endl;
         }
+        break;
     }
-       
+    case 2:
+    {
+        cout << "You are 25 feet away from the basket. You dribble 5 feet to the left. How far are you from the basket?" << endl;
+        cin >> answer2;
+        if (answer2 == 25.5)
+        {
+            cout << "Correct! Calculating probability..." << endl;
+            if (prob1 < 3)
+            {
+                score++;
+                score++;
+                score++;
+                cout << "You scored! You you now have " << score << " points." << endl;
+            }
+            else
+            {
+                cout << "You missed!" << endl;
+            }
+        }
+        else
+        {
+            cout << "You missed!" << endl;
+        }
+        break;
+    }
+    }
 }
 
+void ThirdOffensivePosession()
+{
+    cout << "Third posession: " << endl;
+    cout << "You have been given the ball at the top of the 3 point line which is 22 feet from the basket. You can either shoot the 3 or dribble forward 2 feet and right 2 feet and shoot a 2 point shot." << endl;
+    int selection = PlayerOption();
+    switch (selection)
+    {
+        case 1:
+        {
+            cout << "You dribble to the new position. How far away from the basket are you?" << endl;
+            cin >> answer2;
+            if (answer2 == 20.1)
+            {
+                cout << "Correct! Calculating probability..." << endl;
+                if (prob1 > 2)
+                {
+                    score++;
+                    score++;
+                    cout << "You scored! You now have " << score << " points!" << endl;
+                }
+                else
+                {
+                    cout << "You missed!" << endl;
+                }
+            }
+            else
+            {
+                cout << "You missed!" << endl;
+            }
+            break;
+        }
+        case 2:
+        {
+            cout << "You dribble back 2 feet and to the left 2 feet. How far away from the basket are you?" << endl;
+            cin >> answer2;
+            if (answer == 24.08)
+            {
+                cout << "Correct! Calculating probabilty..." << endl;
+                if (prob1 < 3)
+                {
+                    score++;
+                    score++;
+                    score++;
+                    cout << "You scored! You now have " << score << " points!" << endl;
+                }
+                else
+                {
+                    cout << "You missed!" << endl;
+                }
+            }
+            else
+            {
+                cout << "You missed!" << endl;
+            }
+            break;
+        }
+    }
+}
